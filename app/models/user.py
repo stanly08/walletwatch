571b37da9e7f4c8a49c265686e458c481f43990f
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 00:23:52 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,9 @@
-from .. import db
-from flask_login import UserMixin
+from app import db  # Import db from app/__init__.py
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
     expenses = db.relationship('Expense', backref='user', lazy=True)
+

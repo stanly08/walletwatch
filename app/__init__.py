da16a3040ced871630d2024763d46547761186d9
Stanly Anasi <anasistanly@gmail.com>
2024-09-05 23:49:41 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import secrets
+from config import Config
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 login_manager = LoginManager()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = secrets.token_hex(16)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///walletwatch.db'
-    
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'main.login'
+    app.config.from_object(config_class)
 
-    from .routes import main
-    app.register_blueprint(main)
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    with app.app_context():
+        from . import routes, models
+        db.create_all()
 
     return app
 

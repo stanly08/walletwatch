bbab340860a14190ae46024ffadb9f180e1b6030
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 00:14:23 +0300

    Updated user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,23 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy import Column, String, Integer, ForeignKey
+from .. import db
 from flask_login import UserMixin
 
-db = SQLAlchemy()
-
-class User(UserMixin, db.Model):
-    __tablename__ = 'users'
-    
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    
-    # Relationship with Expense model
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
     expenses = db.relationship('Expense', backref='user', lazy=True)
-    
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-    
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)

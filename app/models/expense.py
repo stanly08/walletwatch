dc62b481718ba387d58cc0fbb3aa920f918f4d71
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 00:03:08 +0300

    Created expense.py

---
+++ app/models/expense.py
@@ -0,0 +1,9 @@
+from . import db
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)

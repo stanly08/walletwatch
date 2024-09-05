8e07b0175127c154ce7afe5ab2f51fe8a00c9566
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 00:30:37 +0300

    Update expense.py

--- app/models/expense.py
+++ app/models/expense.py
@@ -7,5 +7,3 @@ class Expense(db.Model):
     amount = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-

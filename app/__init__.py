16f9e9da41686d3eabc28a98a002f92eefac7db0
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 00:21:22 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    db.init_app(app)
 
-    # Import models only after db is initialized
-    from app.models import User, Expense
-    # Import routes or blueprints here
+    db.init_app(app) 
+
+    # Import and register blueprints/routes after db is initialized
     from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 

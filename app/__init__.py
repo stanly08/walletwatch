07d7d2a2a855e09b63c937f94d3c6d66ed3de91f
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 20:44:26 +0300

    corrected __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,14 @@ from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate.init_app(app, db)
     
     login_manager.init_app(app)
     login_manager.login_view = 'main.login'

b847ca61cdc465e7e32c169b94e29430e2b44ce7
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 15:34:36 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
 from flask_migrate import Migrate
+from app.models import User 
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,16 +11,18 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    
     app.config.from_object(Config)
-    
+
     db.init_app(app)
     login_manager.init_app(app)
-    
     login_manager.login_view = 'main.login'
-    
-    # Register blueprints or routes
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     from app.routes import main
     app.register_blueprint(main)
-    
+
     return app
+

4190bdfe08ae9cde6c6ac54a5651bb2e3cb426d5
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 14:44:40 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
 from flask_login import LoginManager
+from config import Config
 
 db = SQLAlchemy()
-
 login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Optionally set the login view
-login_manager.login_view = 'main.login'
-
 
 def create_app():
     app = Flask(__name__)
+    
     app.config.from_object(Config)
-
-    db.init_app(app) 
-
-    # Import and register blueprints/routes after db is initialized
-    from app.routes import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    login_manager.login_view = 'main.login'
+    
+    # Register blueprints or routes
+    from app.routes import main
+    app.register_blueprint(main)
+    
     return app

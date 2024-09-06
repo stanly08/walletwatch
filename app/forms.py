d2526b3dab92b05c497cd0c04f82c51a4fab4996
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 07:29:27 +0300

    Corrected forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, FloatField, SelectF
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from app.models import User
 
-class SignupForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])

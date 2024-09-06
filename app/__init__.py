6a7a680e8da135d1ba0b53426ed1014de92bc75c
Stanly Anasi <anasistanly@gmail.com>
2024-09-06 14:40:05 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Optionally set the login view
+login_manager.login_view = 'main.login'
+
 
 def create_app():
     app = Flask(__name__)

7a25bfdf5e4a7c0614e2be7bb4bd45d123f93a73
Stanly Anasi <anasistanly@gmail.com>
2024-09-12 22:48:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
 class Config:
+    # Secret key setup
     m_key = '0a619fb395d0f3b8b2975cf7fd54593b0316f86bf2dc0517'
     SECRET_KEY = os.environ.get('SECRET_KEY') or m_key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+
+    # SQLite database URI pointing to instance/database.db
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.getcwd(), 'instance', 'database.db')
+
+    # Disable modification tracking for performance
     SQLALCHEMY_TRACK_MODIFICATIONS = False

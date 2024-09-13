d1afb998c1627766f19d2eb7390960a5f350e3d5
Stanly Anasi <anasistanly@gmail.com>
2024-09-13 23:24:10 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request  # Added 'request' here
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db  # Import db from app/__init__.py
